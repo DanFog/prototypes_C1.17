5172777014b6a0b4a9f9a65acee7f755fee71cba
Daniel Fogerty <Danjfogerty@gmail.com>
2017-02-19 15:56:04 -0800

    server is listening on port 8080

--- node_webserver/webserver.js
+++ node_webserver/webserver.js
@@ -1,1 +1,13 @@
+var http = require("http");
 
+function handleRequest(request, response) {
+  console.log('a request was made');
+  response.write('you have hit the server! \n' );
+  response.end('it works! path hit: ' + request.url);
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(8080, function() {
+  console.log('server is listening on port 8080');
+});
